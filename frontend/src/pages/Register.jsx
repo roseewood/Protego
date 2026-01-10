47e991a08cc37eec609845aaccb030d47036c8dd
roseewood <193557890+roseewood@users.noreply.github.com>
2026-01-10 19:31:09 +0530

    added Register Page

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = async () => {
+    const res = await fetch("http://127.0.0.1:8000/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password })
+    });
+
+    const data = await res.json();
+    alert(data.message || "Registered");
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <input placeholder="Email" onChange={e => setEmail(e.target.value)} />
+      <input placeholder="Password" type="password" onChange={e => setPassword(e.target.value)} />
+      <button onClick={register}>Register</button>
+    </div>
+  );
+}
