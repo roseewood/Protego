474662fcf51070b1fbeec544bcbd4faa0be0fe18
roseewood <193557890+roseewood@users.noreply.github.com>
2026-01-10 19:30:37 +0530

    added Home Page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Smart Home Security System</h1>
+      <Link to="/login">Login</Link> |{" "}
+      <Link to="/register">Register</Link>
+    </div>
+  );
+}
